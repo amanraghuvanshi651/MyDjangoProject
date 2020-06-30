de759c07648b088d2acae050aa4e0de15ee03edf
amanraghuvanshi651 <amanraghuvanshi651@gmail.com>
2020-06-30 10:33:39 +0530

    migration feature added

--- articles/models.py
+++ articles/models.py
@@ -6,5 +6,12 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    #add in thumbnail later
+    #add in thumbnail  later
     #add in author later
+
+    #syntax to create and ruun migration
+    #python manage.py makemigration
+    #python manage.py migrate
+
+    def __str__(self):
+        return self.title
